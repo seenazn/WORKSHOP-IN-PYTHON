534e8e4f306c7bcd464078ab26a692288f8a9d70
Seena <167744692+seenazn@users.noreply.github.com>
2025-07-18 12:38:28 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Book
+from .forms import BookForm
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'add_book.html', {'form': form})
